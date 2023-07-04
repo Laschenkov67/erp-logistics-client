14841cf6ae26646a8f68aaf9ef639ae14045437f
Artem Laschenkov <laschenkov59@yandex.ru>
2023-07-04 23:30:58 +0300

    FEAT | ERP LOGISTICS CLIENT | Добавление auth layout

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { AuthLayoutComponent } from './shared/auth-layout/auth-layout.component';
 
 
 @NgModule({
   declarations: [
     SignInComponent,
-    SignUpComponent
+    SignUpComponent,
+    AuthLayoutComponent
   ],
   imports: [
     CommonModule,
