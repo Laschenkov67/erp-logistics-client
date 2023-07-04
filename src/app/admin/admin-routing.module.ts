8942436c9771dcb4a5cf64acbab33d175ef9b8de
Artem Laschenkov <laschenkov59@yandex.ru>
2023-07-04 23:40:27 +0300

    FEAT | ERP LOGISTICS CLIENT | Добавление adminRoutes

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { adminRoutes } from './routes';
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(adminRoutes)],
   exports: [RouterModule]
 })
 export class AdminRoutingModule { }
